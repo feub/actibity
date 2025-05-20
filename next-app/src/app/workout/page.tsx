c036ee01d3626232790d4928ae8048422faa8f83
Fabien Amann <fabien@feub.net>
2025-05-20 17:31:53 +0200

    feat: workouts page with full workouts info

--- next-app/src/app/workout/page.tsx
+++ next-app/src/app/workout/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/database/prisma";
+import Link from "next/link";
 
 export default async function Page() {
   const workouts = await prisma.workout.findMany({
@@ -30,64 +31,78 @@ export default async function Page() {
     },
   });
 
-  console.log("workouts", workouts);
-
   return (
-    <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 
-    font-[family-name:var(--font-geist-sans)]"
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2>Workouts</h2>
-        <ol className="">
-          {workouts.map((workout) => (
-            <li
-              key={workout.id}
-              className="flex flex-col gap-[16px] items-center sm:items-start bg-amber-950 p-[16px] rounded-[8px]"
-            >
-              <h3 className="text-[24px]">
-                {workout.name} (id: {workout.id})
-              </h3>
-              <p className="text-[16px]">Note: {workout.note}</p>
-              <ol>
-                {workout.sets.map((set) => (
-                  <li
-                    key={set.id}
-                    className="flex flex-col gap-[16px] items-center sm:items-start bg-amber-900 p-[16px] rounded-[8px]"
-                  >
-                    <h4 className="text-[20px]">Set (id: {set.id})</h4>
-                    <p className="text-[16px]">reps: {set.reps}</p>
-                    <ol>
-                      {set.ExercisesOnSets.map((exercise) => (
-                        <li
-                          key={exercise.position}
-                          className="flex flex-col gap-[16px] items-center sm:items-start bg-amber-800 p-[16px] rounded-[8px] mb-4"
-                        >
-                          <h5 className="text-[18px]">
-                            {exercise.exercise.name} (id: {exercise.exercise.id}
-                            )
-                          </h5>
-                          <div className="flex gap-[8px]">
-                            <p className="text-[16px]">
-                              weight: {exercise.weight?.toString() || ""}
-                            </p>
-                            <p className="text-[16px]">
-                              reps/time: {exercise.reps_time}
-                            </p>
-                          </div>
-                          <p className="text-[16px]">Note: {exercise.note}</p>
-                        </li>
-                      ))}
-                    </ol>
-                  </li>
-                ))}
-              </ol>
-              <ol>
-                {/* <p className="text-[16px]">setId: {workout.sets.id}</p> */}
-              </ol>
-            </li>
-          ))}
-        </ol>
+    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen gap-4">
+      <header className="row-start-1">Header</header>
+      <main className="row-start-2 bg-zinc-900">
+        <div className="max-w-4xl mx-auto flex flex-col gap-4 items-center sm:items-start px-4">
+          <h2>Workouts</h2>
+          <ol className="w-full">
+            {workouts.map((workout) => (
+              <li
+                key={workout.id}
+                className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-950 p-4 rounded-xl mb-4"
+              >
+                <h3 className="text-3xl">
+                  <Link href={`/workout/${workout.id}/view`}>
+                    {workout.name}
+                  </Link>
+                </h3>
+                {workout.note && (
+                  <p className="text-sm text-amber-700">Note: {workout.note}</p>
+                )}
+                <ol className="w-full">
+                  {workout.sets.map((set) => (
+                    <li
+                      key={set.id}
+                      className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-900 p-4 rounded-xl mb-4"
+                    >
+                      <div className="w-full flex justify-between items-center">
+                        <h4 className="text-2xl">
+                          Set to be repeated {set.reps} times
+                        </h4>
+                        <p className="text-sm text-amber-700">id: {set.id}</p>
+                      </div>
+                      <ol className="w-full">
+                        {set.ExercisesOnSets.map((exercise) => (
+                          <li
+                            key={exercise.position}
+                            className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-800 p-4 rounded-lg mb-4"
+                          >
+                            <div className="w-full flex justify-between items-center">
+                              <h5 className="text-xl">
+                                {exercise.exercise.name}
+                              </h5>
+                              <div className="flex flex-row items-between gap-4">
+                                <p className="text-md">
+                                  weight:{" "}
+                                  <span className=" font-bold">
+                                    {exercise.weight?.toString() + " kg" || ""}
+                                  </span>
+                                </p>
+                                <p className="text-md">
+                                  reps:{" "}
+                                  <span className=" font-bold">
+                                    {exercise.reps_time}
+                                  </span>
+                                </p>
+                              </div>
+                            </div>
+                            {exercise.note && (
+                              <p className="text-sm text-amber-600 italic">
+                                Note: {exercise.note}
+                              </p>
+                            )}
+                          </li>
+                        ))}
+                      </ol>
+                    </li>
+                  ))}
+                </ol>
+              </li>
+            ))}
+          </ol>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <p>actibity 2025</p>
