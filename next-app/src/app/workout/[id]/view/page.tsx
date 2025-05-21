017df1d24784636dc7e3b49a2e23d8b7689e9d62
Fabien Amann <fabien@feub.net>
2025-05-21 19:07:51 +0200

    refactor: workout view style

--- next-app/src/app/workout/[id]/view/page.tsx
+++ next-app/src/app/workout/[id]/view/page.tsx
@@ -1,5 +1,6 @@
 import { getWorkoutById } from "@/app/lib/data/workout";
 import Link from "next/link";
+import { lusitana } from "@/app/ui/fonts";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -8,54 +9,62 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const workout = await getWorkoutById(id);
 
   return (
-    <div className="max-w-4xl mx-auto flex flex-col gap-4 items-center sm:items-start px-4">
-      <h2>Workout {workout.name}</h2>
+    <div className="max-w-4xl mx-auto flex flex-col gap-4 items-center sm:items-start p-4">
+      <h3 className={`text-4xl ${lusitana.className} antialiased`}>
+        Workout {workout.name}
+      </h3>
 
-      {workout.note && (
-        <p className="text-sm text-amber-700">Note: {workout.note}</p>
-      )}
-      <ol className="w-full">
-        {workout.sets.map((set) => (
-          <li
-            key={set.id}
-            className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-900 p-4 rounded-xl mb-4"
-          >
-            <div className="w-full flex justify-between items-center">
-              <h4 className="text-2xl">Set to be repeated {set.reps} times</h4>
-              <p className="text-sm text-amber-700">id: {set.id}</p>
-            </div>
-            <ol className="w-full">
-              {set.exercises.map((exercise) => (
-                <li
-                  key={exercise.position}
-                  className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-800 p-4 rounded-lg mb-4"
-                >
-                  <div className="w-full flex justify-between items-center">
-                    <h5 className="text-xl">{exercise.exercise.name}</h5>
-                    <div className="flex flex-row items-between gap-4">
-                      <p className="text-md">
-                        weight:{" "}
-                        <span className=" font-bold">
-                          {exercise.weight?.toString() + " kg" || ""}
-                        </span>
-                      </p>
-                      <p className="text-md">
-                        reps:{" "}
-                        <span className=" font-bold">{exercise.reps_time}</span>
-                      </p>
+      <div className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-950 p-4 rounded-xl mb-4">
+        {workout.note && (
+          <p className="text-sm text-amber-700">Note: {workout.note}</p>
+        )}
+        <ol className="w-full">
+          {workout.sets.map((set) => (
+            <li
+              key={set.id}
+              className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-900 p-4 rounded-xl mb-4"
+            >
+              <div className="w-full flex justify-between items-center">
+                <h4 className="text-2xl">
+                  Set to be repeated {set.reps} times
+                </h4>
+                <p className="text-sm text-amber-700">id: {set.id}</p>
+              </div>
+              <ol className="w-full">
+                {set.exercises.map((exercise) => (
+                  <li
+                    key={exercise.position}
+                    className="w-full flex flex-col gap-4 items-center sm:items-start bg-amber-800 p-4 rounded-lg mb-4"
+                  >
+                    <div className="w-full flex justify-between items-center">
+                      <h5 className="text-xl">{exercise.exercise.name}</h5>
+                      <div className="flex flex-row items-between gap-4">
+                        <p className="text-md">
+                          weight:{" "}
+                          <span className=" font-bold">
+                            {exercise.weight?.toString() + " kg" || ""}
+                          </span>
+                        </p>
+                        <p className="text-md">
+                          reps:{" "}
+                          <span className=" font-bold">
+                            {exercise.reps_time}
+                          </span>
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  {exercise.note && (
-                    <p className="text-sm text-amber-600 italic">
-                      Note: {exercise.note}
-                    </p>
-                  )}
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
+                    {exercise.note && (
+                      <p className="text-sm text-amber-600 italic">
+                        Note: {exercise.note}
+                      </p>
+                    )}
+                  </li>
+                ))}
+              </ol>
+            </li>
+          ))}
+        </ol>
+      </div>
       <p>
         <Link href={`/workout`}>Go back</Link>
       </p>
