79a55a2ae696bf6cec5f7c6450c92d56e9b352a2
Fabien Amann <fabien@feub.net>
2025-05-22 16:22:39 +0200

    chore: dark mode switch tweak for Tailwind

--- next-app/src/app/ui/ModeToggler.tsx
+++ next-app/src/app/ui/ModeToggler.tsx
@@ -7,10 +7,19 @@ import {
   RadioGroup,
   useColorScheme,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function ModeToggler() {
   const { mode, setMode } = useColorScheme();
+
+  // Sync Material UI theme with Tailwind dark mode
+  useEffect(() => {
+    if (mode === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [mode]);
 
   if (!mode) {
     return null;
@@ -19,7 +28,7 @@ export default function ModeToggler() {
   return (
     <FormControl>
       <RadioGroup
-        aria-labelledby="demo-theme-toggle"
+        aria-labelledby="theme-toggle"
         name="theme-toggle"
         row
         value={mode}
--- next-app/src/theme.ts
+++ next-app/src/theme.ts
@@ -1,21 +1,60 @@
 "use client";
 
 import { createTheme } from "@mui/material/styles";
+// import { orange } from "@mui/material/colors";
+
+// Augment the palette to include an ochre color
+declare module "@mui/material/styles" {
+  interface Palette {
+    orange: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    orange?: PaletteOptions["primary"];
+  }
+}
 
 const theme = createTheme({
   colorSchemes: {
-    dark: true,
-  },
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#171717",
+    light: {
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#f57b00",
+        },
+        secondary: {
+          main: "#ededed",
+        },
+        background: {
+          default: "#ffffff",
+        },
+        orange: {
+          main: "#ffe0b2",
+          light: "#fff3e0",
+          dark: "#ffcb80",
+          contrastText: "#f57b00",
+        },
+      },
     },
-    secondary: {
-      main: "#ededed",
-    },
-    background: {
-      default: "#ffffff",
+    dark: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#fff3e0",
+        },
+        secondary: {
+          main: "#171717",
+        },
+        background: {
+          default: "#0a0a0a",
+        },
+        orange: {
+          main: "#f57b00",
+          light: "#ff9700",
+          dark: "#ef6b01",
+          contrastText: "#fff3e0",
+        },
+      },
     },
   },
   typography: {
