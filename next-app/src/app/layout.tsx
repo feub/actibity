2176b6e98d7eb68b6b4e0ac71e4f7825a05cac26
Fabien Amann <fabien@feub.net>
2025-05-22 11:53:48 +0200

    feat: added ModeToggler component

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
 import { inter } from "@/app/ui/fonts";
 import "./globals.css";
+import Header from "./ui/header";
+import Footer from "./ui/footer";
 
 export const metadata: Metadata = {
   title: "actibity",
-  description: "Build and collect your workouts!",
+  description: "Build and record your workouts!",
 };
 
 export default function RootLayout({
@@ -19,7 +22,12 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <body className={`${inter} antialiased`}>
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
--- next-app/src/theme.ts
+++ next-app/src/theme.ts
@@ -3,6 +3,9 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
   palette: {
     mode: "light",
     primary: {
