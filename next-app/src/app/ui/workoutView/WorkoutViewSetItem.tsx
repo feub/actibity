1f9d3fe2fff3d8f5459418b326163573ac7771e2
Fabien Amann <fabien@feub.net>
2025-05-26 18:25:40 +0200

    fix: client-side rendering for the WorkoutViewSetItem to avoid Timeline glitches

--- next-app/src/app/ui/workoutView/WorkoutViewSetItem.tsx
+++ next-app/src/app/ui/workoutView/WorkoutViewSetItem.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { SetWithExercises } from "@/app/lib/data/workout";
 import Timeline from "@mui/lab/Timeline";
@@ -15,7 +17,7 @@ export default function WorkoutViewSetItem({ set }: { set: SetWithExercises }) {
   return (
     <div className=" bg-zinc-200 dark:bg-zinc-800 rounded-lg rounded-bl-4xl rounded-tr-4xl p-4 mb-4">
       <div className="w-full flex justify-between items-center">
-        <h4 className="text-2xl mb-4">
+        <h4 className="text-lg mb-4">
           Set to be repeated{" "}
           <span className="text-lime-600 dark:text-lime-400 font-bold">
             {set.reps}
