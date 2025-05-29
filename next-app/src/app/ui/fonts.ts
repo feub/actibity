65f3ff3cd413ef339234530452ccd855dcce9d28
Fabien Amann <fabien@feub.net>
2025-05-29 12:01:22 +0200

    feat: new font Barlow

--- next-app/src/app/ui/fonts.ts
+++ next-app/src/app/ui/fonts.ts
@@ -1,7 +1,11 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Lusitana, Barlow } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
   subsets: ["latin"],
   weight: ["400", "700"],
+});
+export const barlow = Barlow({
+  subsets: ["latin"],
+  weight: ["100", "400", "600", "800", "900"],
 });
